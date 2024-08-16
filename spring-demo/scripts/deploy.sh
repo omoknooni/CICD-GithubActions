672f62701b9f7cf70d7b8fee261eed02c8cd0b63
omoknooni <hihm0609@gmail.com>
2024-08-16 16:34:03 +0900

    codedeploy test1

---
+++ spring-demo/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/spring-demo/*.jar)
+$JAR_NAME=$(basename $BUILD_JAR)
+echo "[*] JAR Path: $BUILD_JAR"
+chmod +x $BUILD_JAR
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+if [ -z $CURRENT_PID ]; then
+  echo "no process"
+else
+  echo "kill $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+nohup java -jar $BUILD_JAR >> /home/ubuntu/spring-demo/deploy.log 2>> /home/ubuntu/spring-demo/deploy_err.log &
+echo "[*] $JAR_NAME deploy success"
