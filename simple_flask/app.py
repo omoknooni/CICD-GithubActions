79867edf644c7c5d078d7bd71b396d5e11b2932c
omoknooni <hihm0609@gmail.com>
2025-04-14 17:11:13 +0900

    simple_web: init

--- simple_flask/app.py
+++ simple_flask/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, CI/CD!"
 
+@app.route("/health")
+def health():
+    return "OK"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
