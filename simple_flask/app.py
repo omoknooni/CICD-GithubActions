319e66ee84239714a462b807e77c233eda5663ef
omoknooni <hihm0609@gmail.com>
2025-04-14 17:15:19 +0900

    simple_web: add sum endpoint

--- simple_flask/app.py
+++ simple_flask/app.py
@@ -10,5 +10,9 @@ def hello():
 def health():
     return "OK"
 
+@app.route("/sum/<int:a>/<int:b>")
+def sum(a, b):
+    return str(a + b)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
